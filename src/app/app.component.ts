36917bea97dabf7b8f72333583f3f839042da034
Raymond2393 <rokwenda090@gmail.com>
2018-03-11 02:20:32 +0300

    add delete button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ export class AppComponent {
   quotes=[
       new Quote(1,'Dont cry because its over,smile because it happened.','Dr.Seuss',new Date(2018,3,14)),
       new Quote(2,'Be yourself, everyone else is taken.','Oscar Wilde',new Date(2018,2,15)),
-      new Quote(3,'two things are infinite:The universe and human stupidity,and am not sure about the universe.','Albert Einstein',new Date(2018,6,9)),
+      new Quote(3,'Two things are infinite:The universe and human stupidity,and am not sure about the universe.','Albert Einstein',new Date(2018,6,9)),
       new Quote(4,'You only leave once,but if you do it right,once is enough.','Mae West',new Date(2018,1,12)),
       new Quote(5,'Be the change that you wish to see in the world.','Mahatma Gandhi',new Date(2018,0,18)),
       new Quote(6,'Dont walk in front of me...i may not follow. Dont walk behind me...i may not lead.Walk beside me..just be my friend.','Albert Camus',new Date(2018,2,14)),
